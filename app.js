4324c2c6065939599b7b7f46508c41e774c2867d
omkarjagtap1011 <126784997+omkarjagtap1011@users.noreply.github.com>
2023-09-14 21:23:59 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+// Import and use the 'submit' route
+const submitRoute = require('./functions/submit'); // Adjust the path as needed
+app.use('/.netlify/functions/submit', submitRoute);
+
+
 if (!process.env.NETLIFY) {
     const server = app.listen(port, () => {
         console.log(`Server is running on port ${port}`);
     });
 }
+
+module.exports = app;
+module.exports.handler = serverless(app);
